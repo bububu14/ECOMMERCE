33258221d96d5f94e1a5a24e6715a793d241a8f4
bububu14 <105796827+bububu14@users.noreply.github.com>
2022-05-26 22:17:24 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,69 @@
+<!--Footer-->
+    <div class="footer">
+        <div class="container text-left">
+            <div class="row">
+                <div class="col-md-4">
+                    <h1>Useful Links</h1>
+                    <p>Privacy Policy</p>
+                    <p>Terms of Use</p>
+                    <p>Return Policy</p>
+                    <p>Discount Coupons</p>
+                </div>
+                <div class="col-md-4">
+                    <h1>Company</h1>
+                    <p>About Us</p>
+                    <p>Contact Us</p>
+                    <p>Career</p>
+                    <p>Affiliate</p>
+                </div>
+                <div class="col-md-4">
+                    <h1>Follow Us On</h1>
+                    <p><i class="fa fa-facebook-official"></i> Facebook</p>
+                    <p><i class="fa fa-youtube-play"></i> Youtube</p>
+                    <p><i class="fa fa-linkedin"></i> Linkedin</p>
+                    <p><i class="fa fa-twitter"></i> Twitter</p>
+                </div>
+            </div>
+        </div>
+</div>
+
+<style>
+	/*--Footer--*/
+
+.footer {
+    background: #EB984E;
+    color: #fff;
+}
+
+.footer h1 {
+    font-size: 15px;
+    margin: 25px 0;
+}
+
+.footer p {
+    font-size: 12px;
+}
+
+.copyright {
+    margin-bottom: -80px;
+    text-align: center;
+    font-size: 15px;
+    padding: 20px;
+	background-color: #222d32;
+}
+
+.fa-heart-o {
+    color: red;
+    font-size: 15px;
+}
+
+.footer hr {
+    margin-top: 10px;
+    background-color: #ccc;
+}
+
+.footer .row .fa {
+    padding-right: 20px;
+    font-size: 15px;
+}
+</style>
---
+++ includes/sidebar.php
@@ -0,0 +1,12 @@
+<div class="row">
+	<div class='box box-solid'>
+	  	<div class='box-header with-border'>
+	    	<h3 class='box-title'><b>Follow us on our Social Media</b></h3>
+	  	</div>
+	  	<div class='box-body'>
+	    	<a class="btn btn-social-icon btn-facebook"><i class="fa fa-facebook"></i></a>
+	    	<a class="btn btn-social-icon btn-instagram"><i class="fa fa-instagram"></i></a>
+
+	  	</div>
+	</div>
+</div>
