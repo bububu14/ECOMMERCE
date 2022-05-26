1d1821aef5e50277cb3ab18c144db3d54a7326c1
bububu14 <105796827+bububu14@users.noreply.github.com>
2022-05-26 22:40:23 +0800

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -13,12 +13,7 @@ Class Database{
  			$this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
  			return $this->conn;
  		}
- 	/* Heroku remote server */
-	$i++;
-	$cfg["Servers"][$i]["host"] = "us-cdbr-east-05.cleardb.net"; //provide hostname
-	$cfg["Servers"][$i]["user"] = "b96b9d7eb836f5"; //user name for your remote server
-	$cfg["Servers"][$i]["password"] = "9f1221d9"; //password
-	$cfg["Servers"][$i]["auth_type"] = "config"; // keep it as config
+ 	
 $pdo = new Database();
  
 ?>
