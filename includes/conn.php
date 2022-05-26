cdcd4588d07160ad7d1d27622c890977f39f2b38
bububu14 <105796827+bububu14@users.noreply.github.com>
2022-05-26 23:06:15 +0800

    Update conn.php

--- includes/conn.php
+++ includes/conn.php
@@ -1,9 +1,9 @@
 <?php
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
+$cleardb_server = $cleardb_url["us-cdbr-east-05.cleardb.net"];
+$cleardb_username = $cleardb_url["b96b9d7eb836f5"];
+$cleardb_password = $cleardb_url["9f1221d9"];
 $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
